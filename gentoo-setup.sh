5f90d811ae9e0fa049142370f4b3e1b79655a3a1
defunsm <salmanhossain500@gmail.com>
2021-01-19 19:18:33 -0500

    partitioning, mounting and installing

--- gentoo-setup.sh
+++ gentoo-setup.sh
@@ -23,23 +23,10 @@ function mount_and_install {
     else
         echo "Skipping installation of stage 3 tarbell..."
     fi
-
 }
 
 function fast_gentoo_chroot {
-    mount --types proc /proc /mnt/gentoo/proc
-    mount --rbind /sys /mnt/gentoo/sys
-    mount --make-rslave /mnt/gentoo/sys
-    mount --rbind /dev /mnt/gentoo/dev
-    mount --make-rslave /mnt/gentoo/dev
-
-    test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
-    mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
-    chmod 1777 /dev/shm
-
-    chroot /mnt/gentoo /bin/bash
-    source /etc/profile
-    export PS1="(chroot) ${PS1}"
+    mount --types proc /proc /mnt/gentoo/proc && mount --rbind /sys /mnt/gentoo/sys && mount --make-rslave /mnt/gentoo/sys && mount --rbind /dev /mnt/gentoo/dev && mount --make-rslave /mnt/gentoo/dev && test -L /dev/shm && rm /dev/shm && mkdir /dev/shm && mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm && chmod 1777 /dev/shm && chroot /mnt/gentoo /bin/bash && source /etc/profile && export PS1="(chroot) ${PS1}"
 }
 
 if ask "Do you want to partition?" N; then

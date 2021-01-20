3f4042608654c6c521ad414a3ceb27c19d8246d9
defunsm <salmanhossain500@gmail.com>
2021-01-19 19:23:59 -0500

    partitioning, mounting and installing

--- gentoo-setup.sh
+++ gentoo-setup.sh
@@ -29,6 +29,8 @@ function fast_gentoo_chroot {
     mount --types proc /proc /mnt/gentoo/proc && mount --rbind /sys /mnt/gentoo/sys && mount --make-rslave /mnt/gentoo/sys && mount --rbind /dev /mnt/gentoo/dev && mount --make-rslave /mnt/gentoo/dev && test -L /dev/shm && rm /dev/shm && mkdir /dev/shm && mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm && chmod 1777 /dev/shm && chroot /mnt/gentoo /bin/bash && source /etc/profile && export PS1="(chroot) ${PS1}"
 }
 
+# partitioning, mounting, installing, and chrooting
+
 if ask "Do you want to partition?" N; then
     partition_drive
 else
